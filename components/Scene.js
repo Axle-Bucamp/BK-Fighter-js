aea8a292db280802d22ee46a5b23c8ee63413b61
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-06 00:14:28 -0600

    Update Scene component to use BurgerCharacter and JeanCharacter

--- components/Scene.js
+++ components/Scene.js
@@ -1,74 +1,45 @@
-import React, { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Box, Plane, OrbitControls, Text } from '@react-three/drei'
-
-const BurgerCharacter = () => {
-  const ref = useRef()
-  useFrame((state, delta) => {
-    ref.current.rotation.y += delta
-  })
-  return (
-    <group position={[-3, 1, 0]}>
-      <Box ref={ref} args={[1, 0.5, 1]} position={[0, 0.5, 0]}>
-        <meshStandardMaterial color="orange" />
-      </Box>
-      <Box args={[1, 0.2, 1]} position={[0, 1, 0]}>
-        <meshStandardMaterial color="saddlebrown" />
-      </Box>
-      <Text position={[0, 1.5, 0]} fontSize={0.5} color="black">
-        Burger
-      </Text>
-    </group>
-  )
-}
-
-const JeanCharacter = () => {
-  const ref = useRef()
-  useFrame((state, delta) => {
-    ref.current.rotation.y -= delta
-  })
-  return (
-    <group position={[3, 1, 0]}>
-      <Box ref={ref} args={[1, 2, 1]} position={[0, 0, 0]}>
-        <meshStandardMaterial color="blue" />
-      </Box>
-      <sphereGeometry args={[0.5, 32, 32]} />
-      <meshStandardMaterial color="peachpuff" />
-      <Text position={[0, 1.5, 0]} fontSize={0.5} color="black">
-        Jean
-      </Text>
-    </group>
-  )
-}
-
-const Floor = () => {
-  return (
-    <Plane args={[10, 10]} rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-      <meshStandardMaterial color="green" />
-    </Plane>
-  )
-}
-
-const Background = () => {
-  return (
-    <Plane args={[20, 10]} rotation={[0, -Math.PI / 2, 0]} position={[5, 5, 0]}>
-      <meshStandardMaterial color="lightblue" />
-    </Plane>
-  )
-}
+import React, { useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Plane } from '@react-three/drei';
+import BurgerCharacter from './BurgerCharacter';
+import JeanCharacter from './JeanCharacter';
 
 const Scene = () => {
-  return (
-    <Canvas camera={{ position: [0, 5, 10] }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <BurgerCharacter />
-      <JeanCharacter />
-      <Floor />
-      <Background />
-      <OrbitControls />
-    </Canvas>
-  )
-}
+  const [burgerState, setBurgerState] = useState('idle');
+  const [jeanState, setJeanState] = useState('idle');
 
-export default Scene
+  const handleKeyDown = (event) => {
+    if (event.key === 'a') setBurgerState('attack');
+    if (event.key === 'h') setBurgerState('hurt');
+    if (event.key === 'j') setJeanState('attack');
+    if (event.key === 'k') setJeanState('hurt');
+  };
+
+  const handleKeyUp = () => {
+    setBurgerState('idle');
+    setJeanState('idle');
+  };
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex="0">
+      <Canvas camera={{ position: [0, 5, 10] }}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        <OrbitControls />
+
+        <Plane args={[20, 20]} rotation-x={-Math.PI / 2} receiveShadow>
+          <meshStandardMaterial color="green" />
+        </Plane>
+
+        <BurgerCharacter position={[-3, 0, 0]} animationState={burgerState} />
+        <JeanCharacter position={[3, 0, 0]} animationState={jeanState} />
+
+        <Plane args={[20, 10]} position={[0, 5, -5]}>
+          <meshStandardMaterial color="lightblue" />
+        </Plane>
+      </Canvas>
+    </div>
+  );
+};
+
+export default Scene;
