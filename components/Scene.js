74b10b7110333e94664c87a4ba2cb8b177407db9
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-05 23:34:56 -0600

    Updated Scene.js to incorporate the new GameUI component

--- components/Scene.js
+++ components/Scene.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Text } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 import BurgerCharacter from './BurgerCharacter';
 import JeanCharacter from './JeanCharacter';
 import { useGameLogic } from './GameLogic';
+import GameUI from './GameUI';
 
 function Floor() {
   return (
@@ -36,22 +37,22 @@ export default function Scene() {
   } = useGameLogic();
 
   return (
-    <Canvas camera={{ position: [0, 5, 10] }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <OrbitControls />
-      <Floor />
-      <Background />
-      <BurgerCharacter position={burgerPosition} rotation={[0, Math.PI / 2, 0]} animationState={burgerState} />
-      <JeanCharacter position={jeanPosition} rotation={[0, -Math.PI / 2, 0]} animationState={jeanState} />
-      
-      {/* Health bars */}
-      <Text position={[-3, 2, 0]} color="black">{`Burger: ${burgerHealth}`}</Text>
-      <Text position={[3, 2, 0]} color="black">{`Jean: ${jeanHealth}`}</Text>
-      
-      {/* Game state display */}
-      {gameState === 'start' && <Text position={[0, 3, 0]} color="black">Press Space to Start</Text>}
-      {gameState === 'end' && <Text position={[0, 3, 0]} color="black">{`${winner} wins!`}</Text>}
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [0, 5, 10] }}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        <OrbitControls />
+        <Floor />
+        <Background />
+        <BurgerCharacter position={burgerPosition} rotation={[0, Math.PI / 2, 0]} animationState={burgerState} />
+        <JeanCharacter position={jeanPosition} rotation={[0, -Math.PI / 2, 0]} animationState={jeanState} />
+      </Canvas>
+      <GameUI 
+        gameState={gameState}
+        burgerHealth={burgerHealth}
+        jeanHealth={jeanHealth}
+        winner={winner}
+      />
+    </>
   );
 }
