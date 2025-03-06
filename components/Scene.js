1c09ba4b15d938e3d66eafbd24894127b6a3cd74
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-05 23:28:58 -0600

    Updated Scene component to use new character components

--- components/Scene.js
+++ components/Scene.js
@@ -1,67 +1,32 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { Box, Plane, OrbitControls, Text } from '@react-three/drei';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Environment } from '@react-three/drei';
+import { BurgerCharacter } from './BurgerCharacter';
+import { JeanCharacter } from './JeanCharacter';
 
-const BurgerCharacter = ({ position }) => {
+function Floor() {
   return (
-    <group position={position}>
-      <Box args={[1, 1, 1]} position={[0, 0.5, 0]}>
-        <meshStandardMaterial color="orange" />
-      </Box>
-      <Box args={[0.8, 0.2, 0.8]} position={[0, 1.1, 0]}>
-        <meshStandardMaterial color="saddlebrown" />
-      </Box>
-      <Text position={[0, 1.5, 0]} fontSize={0.5} color="white">
-        Burger
-      </Text>
-    </group>
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
+      <planeGeometry args={[20, 20]} />
+      <meshStandardMaterial color="#4a9" />
+    </mesh>
   );
-};
+}
 
-const JeanCharacter = ({ position }) => {
+export default function Scene() {
   return (
-    <group position={position}>
-      <Box args={[0.8, 1.5, 0.5]} position={[0, 0.75, 0]}>
-        <meshStandardMaterial color="blue" />
-      </Box>
-      <Sphere args={[0.4, 32, 32]} position={[0, 1.7, 0]}>
-        <meshStandardMaterial color="peachpuff" />
-      </Sphere>
-      <Text position={[0, 2.2, 0]} fontSize={0.5} color="white">
-        Jean
-      </Text>
-    </group>
-  );
-};
-
-const Floor = () => {
-  return (
-    <Plane args={[20, 10]} rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-      <meshStandardMaterial color="green" />
-    </Plane>
-  );
-};
-
-const Background = () => {
-  return (
-    <Plane args={[20, 10]} position={[0, 5, -5]}>
-      <meshStandardMaterial color="skyblue" />
-    </Plane>
-  );
-};
-
-const Scene = () => {
-  return (
-    <Canvas camera={{ position: [0, 5, 10], fov: 60 }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <BurgerCharacter position={[-3, 0, 0]} />
-      <JeanCharacter position={[3, 0, 0]} />
-      <Floor />
-      <Background />
+    <Canvas camera={{ position: [0, 5, 10], fov: 50 }}>
+      <Suspense fallback={null}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        <Environment preset="sunset" />
+        
+        <BurgerCharacter position={[-3, 0, 0]} rotation={[0, Math.PI / 2, 0]} />
+        <JeanCharacter position={[3, 0, 0]} rotation={[0, -Math.PI / 2, 0]} />
+        
+        <Floor />
+      </Suspense>
       <OrbitControls />
     </Canvas>
   );
-};
-
-export default Scene;
+}
