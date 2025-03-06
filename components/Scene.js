d8bfdbad81f90da97d1a1ab758b7f8d992a3183a
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-06 00:21:42 -0600

    Update Scene.js to incorporate GameUI, GameLogic, Background, and ParticleSystem components

--- components/Scene.js
+++ components/Scene.js
@@ -1,44 +1,56 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Plane } from '@react-three/drei';
 import BurgerCharacter from './BurgerCharacter';
 import JeanCharacter from './JeanCharacter';
+import GameUI from './GameUI';
+import { useGameLogic } from './GameLogic';
+import Background from './Background';
+import ParticleSystem from './ParticleSystem';
 
 const Scene = () => {
-  const [burgerState, setBurgerState] = useState('idle');
-  const [jeanState, setJeanState] = useState('idle');
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'a') setBurgerState('attack');
-    if (event.key === 'h') setBurgerState('hurt');
-    if (event.key === 'j') setJeanState('attack');
-    if (event.key === 'k') setJeanState('hurt');
-  };
-
-  const handleKeyUp = () => {
-    setBurgerState('idle');
-    setJeanState('idle');
-  };
+  const {
+    burgerHealth,
+    jeanHealth,
+    gameState,
+    winner,
+    roundTime,
+    burgerAnimation,
+    jeanAnimation,
+    resetGame,
+    startRound,
+  } = useGameLogic();
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex="0">
-      <Canvas camera={{ position: [0, 5, 10] }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <OrbitControls />
+    <>
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <Canvas camera={{ position: [0, 5, 10] }}>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} />
+          <OrbitControls />
 
-        <Plane args={[20, 20]} rotation-x={-Math.PI / 2} receiveShadow>
-          <meshStandardMaterial color="green" />
-        </Plane>
+          <Background />
 
-        <BurgerCharacter position={[-3, 0, 0]} animationState={burgerState} />
-        <JeanCharacter position={[3, 0, 0]} animationState={jeanState} />
+          <Plane args={[20, 20]} rotation-x={-Math.PI / 2} receiveShadow>
+            <meshStandardMaterial color="green" />
+          </Plane>
 
-        <Plane args={[20, 10]} position={[0, 5, -5]}>
-          <meshStandardMaterial color="lightblue" />
-        </Plane>
-      </Canvas>
-    </div>
+          <BurgerCharacter position={[-3, 0, 0]} animationState={burgerAnimation} />
+          <JeanCharacter position={[3, 0, 0]} animationState={jeanAnimation} />
+
+          {gameState === 'fighting' && (
+            <ParticleSystem position={[0, 1, 0]} color="#FFD700" />
+          )}
+        </Canvas>
+      </div>
+      <GameUI
+        gameState={gameState}
+        burgerHealth={burgerHealth}
+        jeanHealth={jeanHealth}
+        winner={winner}
+        onStartGame={startRound}
+      />
+    </>
   );
 };
 
