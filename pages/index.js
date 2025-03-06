31952fabe19e55252c51a6d4e53def6f3c2f887a
Bucamp Axle <47558509+Axle-Bucamp@users.noreply.github.com>
2025-03-06 00:55:03 -0600

    Create pages/index.js with basic R3F setup for brawler game

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
-import { Suspense } from 'react'
 import dynamic from 'next/dynamic'
-import { Canvas } from '@react-three/fiber'
+import React from 'react'
 
-// Dynamically import the Scene component with SSR disabled
 const Scene = dynamic(() => import('../components/Scene'), { ssr: false })
 
 export default function Home() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas>
-        <Suspense fallback={null}>
-          <Scene />
-        </Suspense>
-      </Canvas>
+      <Scene />
     </div>
   )
 }
